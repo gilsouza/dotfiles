694a0e499df6fe1b7451f2814bc6d1b6bce341fa
Gil Souza <gilmarcs@gmail.com>
2024-12-06 18:04:10 -0300

    add asdf

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -3,3 +3,4 @@ zinit light romkatv/powerlevel10k
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
 zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light asdf-vm/asdf
