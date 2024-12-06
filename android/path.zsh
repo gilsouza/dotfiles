b16bb50b0febdd1916937018d7e2ed6f7e658406
Gil Souza <gilmarcs@gmail.com>
2024-12-06 17:31:07 -0300

    remove rbenv

--- android/path.zsh
+++ android/path.zsh
@@ -3,5 +3,3 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$HOME/dev/flutter/bin
-export PATH=$PATH:$HOME/.rbenv/shims
