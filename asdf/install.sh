4c6936532e7247ceb131fdfc82f38454e485af79
Gil Souza <gilmarcs@gmail.com>
2024-12-07 09:26:43 -0300

    only install asdf if needed

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,14 +1,19 @@
 echo "› Installing asdf"
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 
-# see more on https://github.com/asdf-community/asdf-python
-asdf plugin add python
+if [[ ! -d ~/.asdf/ ]]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 
-# see more on https://github.com/asdf-vm/asdf-nodejs
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  # see more on https://github.com/asdf-community/asdf-python
+  asdf plugin add python
 
-# see more on https://github.com/asdf-vm/asdf-ruby
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  # see more on https://github.com/asdf-vm/asdf-nodejs
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-# see more on https://github.com/halcyon/asdf-java
-asdf plugin-add java https://github.com/halcyon/asdf-java.git
+  # see more on https://github.com/asdf-vm/asdf-ruby
+  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+  # see more on https://github.com/halcyon/asdf-java
+  asdf plugin-add java https://github.com/halcyon/asdf-java.git
+fi
+
+
