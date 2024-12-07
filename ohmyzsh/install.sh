4130ae30983226529d9837361a60c0e93f4d79db
Gil Souza <gilmarcs@gmail.com>
2024-12-07 10:06:56 -0300

    fix zinit installation

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # Install oh-my-zsh
+# https://github.com/ohmyzsh/ohmyzsh
 if [[ ! -d ~/.oh-my-zsh/ ]]; then
   echo "› Installing oh-my-zsh"
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Install Zinit
+# https://github.com/zdharma-continuum/zinit
 if [[ ! -d ~/.local/share/zinit/ ]]; then
   bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 else
