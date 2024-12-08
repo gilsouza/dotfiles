7f31c9f9d78dcfc4d9760f67748ce2a2393ea502
Gil Souza <gilmarcs@gmail.com>
2024-12-08 11:14:34 -0300

    update set defaults for macos

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -20,7 +20,43 @@ chflags nohidden ~/Library
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
+# Set language and text formats
+defaults write NSGlobalDomain AppleLanguages -array "en-BR" "pt-BR"
+defaults write NSGlobalDomain AppleLocale -string "en_BR"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Set Dock 
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-time-modifier -float 0.5
+defaults write com.apple.dock autohide-delay -float 0.2
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock orientation -string "bottom"
+defaults write com.apple.dock largesize -int 16
+defaults write com.apple.dock magnification -bool false
+
+# Set Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+
+# keep folders on top when sorting
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Set Menu Bar
+defaults write com.apple.menuextra.clock DateFormat -string "\"EEE h:mm:ss\""
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
+
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
+sudo systemsetup -settimezone "America/Sao_Paulo" > /dev/null
